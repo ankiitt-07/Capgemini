5cd800aaf71e5ef7952984ee877c8eb413bb0fdf
ankiitt-07 <ak077636@gmail.com>
2025-01-30 11:14:13 +0530

    Added count of Capital char ,lowe , digit, Special char

--- Day4/CountOfLoweCapitalDigitSpecialch.java
+++ Day4/CountOfLoweCapitalDigitSpecialch.java
@@ -2,22 +2,29 @@ import java.util.*;
 public class CountOfLoweCapitalDigitSpecialch {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the String : ");
         String str = sc.nextLine();
         countCharacters(str);
 
     }
     public static void countCharacters(String str){
+        // Here initialising all count as 0 
         int lowerCaseCount=0; int upperCaseCount =0; int digitCount =0; int specialCharCount=0;
+        // Converting String to char  array and traversing 
         for(char ch : str.toCharArray()){
+            // Here checking if the char is upper case if yes then increase count of uppercase 
             if(Character.isUpperCase(ch)){
                 upperCaseCount++;
             }
+            // here checking the char is lower case if yes then increase the count of lowe count 
             else if(Character.isLowerCase(ch)){
                 lowerCaseCount++;
             }
+            // here checking the char is Special charcter or not if yes then increase the count of specialchar count 
             else if(!Character.isLetterOrDigit(ch) && !Character.isWhitespace(ch)){
                 specialCharCount++;
             }
+            // Here checking the char is digit or not if yes then increase the count of digit count 
             else if(Character.isDigit(ch)){
                 digitCount++;
             }
